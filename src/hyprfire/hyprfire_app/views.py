50ffd2ed2a84e0b53bb4f559e5079e92f77562d4
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-04-02 16:44:05 +0800

    Display unique .svg image for each filename in list

--- src/hyprfire/hyprfire_app/views.py
+++ src/hyprfire/hyprfire_app/views.py
@@ -1,9 +1,12 @@
+import base64
+
+from django.http import FileResponse, HttpResponse, JsonResponse
 from django.views import generic
 from os import listdir
 
 
 class IndexView(generic.ListView):
-    template_name = 'hyprfire_app/file_list.html'
+    template_name = 'hyprfire_app/index.html'
 
     def get_queryset(self):
         file_list = listdir()
@@ -15,5 +18,7 @@ class IndexView(generic.ListView):
         return filenames
 
 
-class DetailsView(generic.TemplateView):
-    template_name = 'hyprfire_app/details.html'
+def send_image(request, slug):
+    name = f"{slug}-graph.svg"
+    with open(f'hyprfire_app/static/hyprfire_app/{name}', 'rb') as f:
+        return HttpResponse(f.read(), content_type="image/svg+xml")
