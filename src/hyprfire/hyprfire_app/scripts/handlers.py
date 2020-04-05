929d3e080b034674896140a4c25b5e52be0db4b6
dean_linux <17093877@student.curtin.edu.au>
2020-04-05 20:48:55 +0800

    basic requestHandler with what we have so far, will be changed as converters develop

--- src/hyprfire/hyprfire_app/scripts/handlers.py
+++ src/hyprfire/hyprfire_app/scripts/handlers.py
@@ -1,10 +1,8 @@
 from hyprfire_app.scripts.pcapconverter import pcapConverter
 from hyprfire_app.scripts.tcpdumpconverter import tcpdumpConverter
-import sys
+from hyprfire_app.scripts.n2dconverter import convert
 
 def requestHandler(filename):
     pcapConverter(filename)
     tcpdumpConverter(filename)
-
-if __name__ == '__main__':
-    requestHandler()
+    convert(filename)
