7eb012dce1ab5438cb3401a678f1c71ebd31b83c
dean_linux <17093877@student.curtin.edu.au>
2020-04-05 15:01:23 +0800

    pcapConverter now works correctly, output is put into a tcpd file for now

--- src/hyprfire/hyprfire_app/scripts/pcapconverter.py
+++ src/hyprfire/hyprfire_app/scripts/pcapconverter.py
@@ -1,14 +1,14 @@
 # pcapconverter.py: takes in a filename for a pcap file and writes the tcpdump contents to a .tcpd file
 # Author: Dean Quaife
-# Last edited: 2020/03/30
+# Last edited: 2020/04/05
 import subprocess
 
 
 def pcapConverter(filename):
     pcapFilename = filename + ".pcap"
     tcpdumpFilename = filename + ".tcpd"
-    p = subprocess.Popen(('tcpdump', '-nnr', pcapFilename), shell=True, stdout=subprocess.PIPE)
-    f = open(tcpdumpFilename, 'w')
-    for row in iter(p.stdout.readline, b''):
-        f.write(str(row))
-    f.close()
+    cmd = "tcpdump -nnr " + pcapFilename
+    #p = subprocess.Popen(('tcpdump', '-nnr', '-l', pcapFilename), stdout=subprocess.PIPE)
+    with open(tcpdumpFilename, 'wb') as f:
+        f.write(subprocess.check_output(cmd, shell=True))
+        f.close()
