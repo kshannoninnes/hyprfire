36b637b881e64593cbf3539b422164fc5648ba5f
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-04-28 12:38:18 +0800

    Fix bug with submit route, move filename code to func

--- src/hyprfire/hyprfire_app/views.py
+++ src/hyprfire/hyprfire_app/views.py
@@ -10,6 +10,7 @@ blacklist = [
 
 
 def index(request):
+    filenames = get_filenames()
     if request.method == "POST":
         form = AnalyseForm(request.POST)
         if form.is_valid():
@@ -18,11 +19,15 @@ def index(request):
             length = form.cleaned_data['length']
     else:
         form = AnalyseForm()
-        file_list = os.listdir(monitored_dir)
-        filenames = []
-        for file in file_list:
-            name = file.title().lower()
-            if name not in blacklist:
-                filenames.append(os.path.splitext(name)[0])
     return render(request, 'hyprfire_app/index.html', {'form': form, 'filenames': filenames})
 
+
+def get_filenames():
+    file_list = os.listdir(monitored_dir)
+    filenames = []
+    for file in file_list:
+        name = file.title().lower()
+        if name not in blacklist:
+            filenames.append(os.path.splitext(name)[0])
+
+    return filenames
