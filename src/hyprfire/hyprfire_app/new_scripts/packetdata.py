a7f6617b7adbf31992db71f6ca613cdff4b4b54b
Dean Quaife <deanquaife@outlook.com>
2020-05-08 13:12:06 +0800

    Removed filtering and unneeded PacketData fields. Adjusted test to match

--- src/hyprfire/hyprfire_app/new_scripts/packetdata.py
+++ src/hyprfire/hyprfire_app/new_scripts/packetdata.py
@@ -1,20 +1,14 @@
-# packetdata.py: contains class information for a TCP/UDP packet
+# packetdata.py: contains class information for a packet
 # Will be used in pcapconverter to represent each packet and be passed to n2dconverter
 # Author: Dean Quaife
-# Last edited: 2020/04/27
+# Last edited: 2020/05/07
 
-#represents a TCP/UDP packet
+#represents a packet
 class PacketData:
-    def __init__(self, epochTimestamp, timestamp, IPFrom, IPTo, portFrom, portTo, len, winLen, flags):
+    def __init__(self, epochTimestamp, timestamp, len):
         self.epochTimestamp = epochTimestamp
         self.timestamp = timestamp #this value is microseconds since midnight on day of capture
-        self.IPFrom = IPFrom
-        self.IPTo = IPTo
-        self.portFrom = portFrom
-        self.portTo = portTo
         self.len = len
-        self.winLen = winLen
-        self.flags = flags
 
     #required for comparing packets in testing
     def __eq__(self, other):
