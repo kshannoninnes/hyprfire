82e0406d0e35d4fcc55612249cd4a029c4583ea6
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-05-11 01:01:01 +0800

    Update testing.py for new test file location

--- src/hyprfire/hyprfire_app/utils/testing.py
+++ src/hyprfire/hyprfire_app/utils/testing.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from hyprfire.settings import BASE_DIR
+from .misc import EXPORTED_PCAP_DIR
 
-TEST_FILE = str(Path(__file__).parent.parent.parent / 'pcaps' / 'testdump')
+TEST_FILE = str(Path(BASE_DIR) / 'hyprfire_app' / 'tests' / 'test_files' / 'testdump')
 
 
 def remove_test_files():
@@ -9,9 +11,6 @@ def remove_test_files():
 
     Removes all test files created during testing
     """
-    proj_root = Path(__file__).parent.parent.parent
-    output_dir = proj_root / 'pcaps' / 'exported_pcaps'
-
-    for path in output_dir.glob('*'):
+    for path in EXPORTED_PCAP_DIR.glob('*'):
         if path.is_file():
             path.unlink()
