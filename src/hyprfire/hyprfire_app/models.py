8a3d568c1e57d1f0375ffbccf824066d18c1c8a8
Ren <19168515@student.curtin.edu.au>
2020-05-13 17:41:59 +0800

    Created the postgres Database and Models
    Merge branch 'release/sprint-2' of https://bitbucket.org/ComputingSEP/2020-23-stefan-cyber/src/master
    
    # Conflicts:
    #	src/hyprfire/hyprfire_app/tests/tests.py

--- src/hyprfire/hyprfire_app/models.py
+++ src/hyprfire/hyprfire_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Data(models.Model):
+
+    filename = models.TextField(primary_key=True)
+    algorithm = models.TextField()
+    window_size = models.IntegerField()
+    data = models.TextField()
