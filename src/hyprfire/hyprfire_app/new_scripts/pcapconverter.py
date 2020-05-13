6ddc285cf8423522ea3c30d1f5ae437a1283e3e1
Dean Quaife <deanquaife@outlook.com>
2020-05-13 12:12:43 +0800

    Tried speeding it up with multiprocessing Manager(); doesn't work.

--- src/hyprfire/hyprfire_app/new_scripts/pcapconverter.py
+++ src/hyprfire/hyprfire_app/new_scripts/pcapconverter.py
@@ -7,27 +7,51 @@
 # Last edited: 2020/05/07
 
 from scapy.utils import PcapReader, Scapy_Exception
-from scapy.layers.inet import IP, TCP, UDP
 from hyprfire_app.new_scripts.packetdata import PacketData
 from hyprfire_app.new_scripts.dumpfile import Dumpfile
+from scapy.layers.inet import IP, TCP, UDP #required import for PcapReader to work correctly even though it isn't used
 from hyprfire_app.new_scripts.converterexception import ConverterException
-import datetime
+import datetime, multiprocessing, math
 
 #main pcapconverter method; takes in a pcap filename and returns a Dumpfile object
 def pcapConverter(filename):
-    packets = []
     try:
-        for packet in PcapReader(filename):
-            epochTimestamp = float(packet.time)
-            timestamp = sinceMidnight(epochTimestamp) #used to match timestamp to Stefan's original script
-            len = packet.wirelen
-            nextPkt = PacketData(epochTimestamp, timestamp, len)
-            packets.append(nextPkt)
+        with multiprocessing.Manager() as manager:
+            cores = multiprocessing.cpu_count()
+            dataList = []
+            packetList = []
+            segList = manager.list()
+            proList = []
+            inputQ = multiprocessing.Queue()
+            for packet in PcapReader(filename):
+                packetList.append(packet)
+            segmentSize = math.ceil(len(packetList)/cores)
+            for i in range(0, len(packetList), segmentSize):
+                inputQ.put(packetList[i:i + segmentSize])
+            for i in range(cores):
+                proList.append(multiprocessing.Process(target=extractData, args=(inputQ, segList)))
+            for p in proList:
+                p.start()
+            for p in proList:
+                p.join()
+            for i in segList:
+                dataList += i
     except FileNotFoundError:
         raise ConverterException("Filename does not exist")
     except Scapy_Exception:
         raise ConverterException("Filename is not a supported capture file")
-    return Dumpfile(filename, packets)
+    return dataList
+
+def extractData(inputQ, dataList):
+    segment = inputQ.get()
+    packets = []
+    for packet in segment:
+        epochTimestamp = float(packet.time)
+        timestamp = sinceMidnight(epochTimestamp)  # used to match timestamp to Stefan's original script
+        len = packet.wirelen
+        nextPkt = PacketData(epochTimestamp, timestamp, len)
+        packets.append(nextPkt)
+    dataList.append(packets)
 
 #returns number of microseconds since midnight on the day the packet arrived; matches Stefan's original script
 def sinceMidnight(epochTimestamp):
