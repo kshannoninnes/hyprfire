73ba93cae59511ebfd831dc39f0770f3d8c1936a
Junly Lau <18829167@student.curtin.edu.au>
2020-05-17 12:32:25 -0700

    settings.py and setup.py minor changes to accommodate for install.py deployment

--- src/hyprfire/setup.py
+++ src/hyprfire/setup.py
@@ -24,5 +24,7 @@ setup(
     ],
     include_package_data=True,
 )
-os.makedirs('logs', exist_ok=True)
-os.makedirs('pcaps', exist_ok=True)
+# oldmask = os.umask(000)
+# os.makedirs('logs', exist_ok=True, mode=0o775)
+# os.makedirs('pcaps', exist_ok=True, mode=0o775)
+# os.umask(oldmask)
