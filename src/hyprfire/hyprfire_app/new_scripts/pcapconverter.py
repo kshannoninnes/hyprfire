bddc6e70ae2882d643f48f9de2c261c1a21a1583
deanquaife <deanquaife@outlook.com>
2020-05-19 17:45:37 +0800

    Refactoring, commenting, 100% test coverage

--- src/hyprfire/hyprfire_app/new_scripts/pcapconverter.py
+++ src/hyprfire/hyprfire_app/new_scripts/pcapconverter.py
@@ -1,23 +1,25 @@
-# pcapconverter.py: converts packets from a pcap file into a list of PacketData objects
-# representing that file.
-# pcapConverter takes a filename (with extension) and calls editcapSplit, which uses editcap
-# (a command provided by Wireshark) to split the file into smaller files of 10000 packets each.
-# It then uses a pool of processes to run the extractData method on each of them concurrently.
-# extractData calls the Scapy library method PcapReader to obtain information about each packet and
-# convert it into a list of PacketData objects to be provided to packetdataConverter. The data from
-# each smaller file is compiled together and sorted, then returned.
-# Author: Dean Quaife
-# Last edited: 2020/05/18
+"""
+pcapconverter.py: converts packets from a pcap file into a list of PacketData objects
+representing that file.
+pcapConverter takes a filename (with extension) and calls editcapSplit, which uses editcap
+(a command provided by Wireshark) to split the file into smaller files of 10000 packets each.
+It then uses a pool of processes to run the extractData method on each of them concurrently.
+extractData calls the Scapy library method PcapReader to obtain information about each packet and
+convert it into a list of PacketData objects to be provided to packetdataConverter. The data from
+each smaller file is compiled together and sorted, then returned.
+Author: Dean Quaife
+Last edited: 2020/05/19
+"""
 
 from scapy.all import PcapReader
 from hyprfire_app.new_scripts.packetdata import PacketData
-from hyprfire_app.new_scripts.converterexception import ConverterException
+from hyprfire_app.exceptions import ConverterException
 import datetime, subprocess, multiprocessing
 from pathlib import Path
 from decimal import Decimal
 
-# retrieves data from a pcap file. Returns a PacketData list
-# will raise a ConverterException if filename doesn't exist or isn't a pcap file
+""" retrieves data from a pcap file. Returns a PacketData list
+will raise a ConverterException if filename doesn't exist or isn't a pcap file """
 def pcapConverter(filename):
     cores = multiprocessing.cpu_count()
     temp_paths = []
@@ -41,8 +43,8 @@ def pcapConverter(filename):
         Path(x).unlink() #delete the temporary smaller files
     return data_list
 
-# uses the editcap command provided by Wireshark to split filename into 10000 packet pcap files
-# subprocess.run() will raise an exception if filename is not a pcap file
+""" uses the editcap command provided by Wireshark to split filename into 10000 packet pcap files
+subprocess.run() will raise an exception if filename is not a pcap file """
 def editcapSplit(filename):
     temp = "temp.pcap"
     command = f'editcap -c 10000 {filename} {temp}'
@@ -51,15 +53,16 @@ def editcapSplit(filename):
 # converts important data from each editcap file into a list of PacketData objects
 def extractData(file):
     packets = []
-    for packet in PcapReader(file):
-        epochTimestamp = Decimal(str(packet.time))
-        timestamp = sinceMidnight(epochTimestamp)
-        len = packet.wirelen
-        packets.append(PacketData(epochTimestamp, timestamp, len))
+    with PcapReader(file) as reader:
+        for packet in reader:
+            epochTimestamp = Decimal(str(packet.time))
+            timestamp = sinceMidnight(epochTimestamp)
+            len = packet.wirelen
+            packets.append(PacketData(epochTimestamp, timestamp, len))
     return packets
 
-# returns number of microseconds since midnight on the day the packet arrived
-# used to calculate graph values later
+""" returns number of microseconds since midnight on the day the packet arrived
+used to calculate graph values later """
 def sinceMidnight(epochTimestamp):
     date = datetime.datetime.fromtimestamp(epochTimestamp) #only returns time up to seconds
     seconds_midnight = datetime.datetime(date.year, date.month, date.day).timestamp()
--- src/hyprfire/hyprfire_app/tests/scripts/test_pcapconverter.py
+++ src/hyprfire/hyprfire_app/tests/scripts/test_pcapconverter.py
@@ -1,33 +1,63 @@
-import unittest
+from unittest import TestCase
 from hyprfire_app.new_scripts.converterexception import ConverterException
-from hyprfire_app.new_scripts.pcapconverter import pcapConverter
+from hyprfire_app.new_scripts import pcapconverter
 from hyprfire.settings import BASE_DIR
 from pathlib import Path
-import time
+from decimal import Decimal
 
-class MyTestCase(unittest.TestCase):
+class TestPcapConverter(TestCase):
+    """ test pcapConverter in normal conditions; checks timestamps of the first packet, the first packet
+    of the second file created by editcap, and the last packet against their equivalents in Wireshark
+    also checks the number of packets against Wireshark, and whether the temporary files created during
+    the process have successfully been deleted """
     def test_pcapconverter(self):
         testpath = str(Path(BASE_DIR)/'hyprfire_app'/'tests'/'test_files'/'testdump')
-        starttime = time.time()
-        result = pcapConverter(testpath)
-        runtime = time.time() - starttime
-        expectedPackets = 11850
-        self.assertEqual(1588259849.166453227, float(result[0].epochTimestamp), "First segment")
-        self.assertEqual(1588259885.838919690, float(result[10000].epochTimestamp), "2nd")
-        self.assertEqual(1588259889.284641293, float(result[-1].epochTimestamp), "last")
-        self.assertEqual(expectedPackets, len(result), "Number of packets after filtering")
+        result = pcapconverter.pcapConverter(testpath)
+        exists = False
+        for x in Path(str(Path(BASE_DIR)/'hyprfire_app')).glob('temp*.pcap'):
+            if x.is_file():
+                exists = True
 
-    #test using a non-existant filename
+        self.assertEqual(1588259849.166453227, float(result[0].epochTimestamp))
+        self.assertEqual(1588259885.838919690, float(result[10000].epochTimestamp))
+        self.assertEqual(1588259889.284641293, float(result[-1].epochTimestamp))
+        self.assertEqual(11850, len(result))
+        self.assertFalse(exists)
+
+    #test pcapConverter using a non-existant filename; should throw an exception
     def test_pcapconverter_nofile(self):
         with self.assertRaises(ConverterException) as cm:
-            pcapConverter("fake.pcap")
+            pcapconverter.pcapConverter("fake.pcap")
         e = cm.exception
+
         self.assertEqual(e.message, "Filename does not exist")
 
-    #test using a file that isn't a pcap file
+    #test pcapConverter using a file that isn't a pcap file; should throw an exception
     def test_pcapconverter_wrongfile(self):
         testpath = str(Path(BASE_DIR)/'hyprfire_app'/'new_scripts'/'pcapconverter.py')
         with self.assertRaises(ConverterException) as cm:
-            pcapConverter(testpath)
+            pcapconverter.pcapConverter(testpath)
         e = cm.exception
+
         self.assertEqual(e.message, "Editcap failure: is this file a capture file?")
+
+    """ test extractData under normal conditions; checks timestamps of first and last packets against
+    their equivalents in Wireshark, along with the total number of packets
+    Note that pcapConverter should throw exceptions for incorrect filenames or filetypes before
+    reaching extractData """
+    def test_extractData(self):
+        testpath = str(Path(BASE_DIR) / 'hyprfire_app' / 'tests' / 'test_files' / 'testdump')
+        result = pcapconverter.extractData(testpath)
+
+        self.assertEqual(1588259849.166453227, float(result[0].epochTimestamp), "First segment")
+        self.assertEqual(1588259889.284641293, float(result[-1].epochTimestamp), "last")
+        self.assertEqual(11850, len(result))
+
+    """ test sinceMidnight
+    Note that Decimal creates some strange rounding issues, so the number passed into sinceMidnight is
+    actually 1588259850.7007715702056884765625, so result is tested accordingly """
+    def test_sinceMidnight(self):
+        val = Decimal(1588259850.700771485)
+        result = pcapconverter.sinceMidnight(val)
+
+        self.assertEqual(83850700771.5702056884765625, result)
